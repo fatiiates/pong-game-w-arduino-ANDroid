73161e5f59f510dc71a106747f7a21ed5833c1a4
Fatih Ateş <fatiiates@gmail.com>
2021-05-17 20:20:48 +0300

    Pong Game Released

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial'
-import { NavController, LoadingController, AlertController, ToastController } from 'ionic-angular'
+import { NavController, LoadingController, AlertController, ToastController, Loading } from 'ionic-angular'
 
 @Component({
 	selector: 'page-home',
@@ -11,50 +11,60 @@ export class HomePage {
 	pairedList: pairedlist
 	listToggle: boolean = false
 	pairedDeviceID: number = 0
-	isOpened: boolean = false
+	isOpened: boolean = true
 	dataSend: string = ""
 	isConnected: boolean = false
+	locationX: number = 0
 
 	constructor(public navCtrl: NavController, public loadingController: LoadingController, private alertCtrl: AlertController, private bluetoothSerial: BluetoothSerial, private toastCtrl: ToastController) {
 		this.checkBluetoothEnabled()
 	}
 
-	presentLoadingWithOptions() {
+	presentLoadingWithOptions(content = "İşleminiz gerçekleştiriliyor..."): Loading {
 		const loading =  this.loadingController.create({
 		  spinner: null,
-		  duration: 5000,
-		  content: 'Click the backdrop to dismiss early...',
+		  content: content,
 		  cssClass: 'custom-class custom-loading',
-		});
-		loading.present();
+		})
+		loading.present()
 	
 		loading.onDidDismiss(function(data, role){
-			console.log('Loading dismissed with role:', role);
-		});
+			console.log('Loading dismissed with role:', role)
+		})
 		
+		return loading
 	}
 
 	checkBluetoothEnabled() {
-		this.presentLoadingWithOptions();
+		const loading = this.presentLoadingWithOptions()
 		this.bluetoothSerial.isEnabled()
+			.then(() => {
+				loading.dismiss()
+			})
 			.catch(error => {
-				this.showError("Lütfen Bluetooth aracınızı aktif hale getirin.")
+				loading.dismiss()
+				this.showError(error)
+				//this.showError("Lütfen Bluetooth aracınızı aktif hale getirin.")
 			})
 	}
 
 	listPairedDevices() {
+		const loading = this.presentLoadingWithOptions()
 		this.bluetoothSerial.list()
 			.then(success => {
 				this.pairedList = success
 				this.listToggle = true
+				loading.dismiss()
 			})
 			.catch(error => {
-				this.showError(".\n")
+				loading.dismiss()
+				this.showError(error + ".\n")
 				this.listToggle = false
 			})
 	}
 
 	selectDevice() {
+
 		let connectedDevice = this.pairedList[this.pairedDeviceID]
 		if (!connectedDevice.address) {
 			this.showError('Eşleştirmek istediğiniz cihaz seçiniz.')
@@ -67,14 +77,21 @@ export class HomePage {
 
 	connect(address) {
 	// Attempt to connect device with specified address, call app.deviceConnected if success
-		this.bluetoothSerial.connect(address)
+		const loading = this.presentLoadingWithOptions()
+		var trest = this.bluetoothSerial.connect(address)
 			.subscribe(success => {
 				this.deviceConnected()
+				loading.dismiss()
 				this.showToast("Bluetooth bağlantısı başarılı!")
 				this.isConnected = true
+				
 			}, error => {
-				this.showError(error)
+				if(loading)
+					loading.dismiss()
+				this.isConnected = false
+				this.showError("Cihaz ile telefon arasındaki bağlantı koparıldı.")
 			})
+			trest
 	}
 
 	deviceConnected() {
@@ -110,12 +127,35 @@ export class HomePage {
 			})
 	}
 
+	moveTo(param: string) {
+		var newLocation: number = this.locationX
+		if(param == 'Right')
+			newLocation += 1
+		else if(param == 'Left')
+			newLocation -= 1
+		else 
+			return 0
+
+		if(newLocation > 5 || newLocation < 0){
+			this.showToast('Sınırdasınız')
+			return 0
+		}
+
+		this.bluetoothSerial.write(newLocation.toString())
+			.then(success => {			
+				this.locationX = newLocation
+			})
+			.catch(error => {
+				this.showError(error)
+			})
+	}
+
 	showError(error) {
 		let alert = this.alertCtrl.create({
 			title: 'Hata',
 			subTitle: error,
 			buttons: ['Kapat']
-		});
+		})
 		alert.present()
 	}	
 
@@ -123,7 +163,7 @@ export class HomePage {
 		const toast = this.toastCtrl.create({
 			message: msj,
 			duration: 1000
-		});
+		})
 		toast.present()
 	}
 
